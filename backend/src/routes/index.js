e447c64fe3f8c6ea20ad112416c53c32b98f376b
Adriele Antunes <luiz_henrique_93@outlook.com>
2026-07-24 22:23:42 -0300

    create user model

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { success } = require('../utils/response');
 const healthController = require('../controllers/health.controller');
+const userRoutes = require('./user.routes');
 
 router.get('/', (req, res) => {
   success(res, 200, 'LionStock API', {
@@ -11,5 +12,8 @@ router.get('/', (req, res) => {
 });
 
 router.get('/health', healthController.checkHealth);
+
+// mount auth & user routes under /api
+router.use('/api', userRoutes);
 
 module.exports = router;
